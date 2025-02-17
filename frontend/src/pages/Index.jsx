3ff83b5067d54727f46a471a33f8abe4dacec98d
websitecuakhoi <khoitk14033@gmail.com>
2025-02-17 11:27:56 +0700

    Change update

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -2,10 +2,10 @@ import React from "react";
 const Index = () => {
   return (
     <div className="bg-[#18191b] min-h-screen w-full ">
-      <div className="bg-[#212223] shadow-md">
+      <div className="bg-[#ccced0] shadow-md">
         <div className="w-[93%] m-auto py-3">
           <div className="flex justify-between items-center">
-            <div className="w-[100px] h-[100px]">
+            <div className="w-[80px] h-[80px]">
               <img
                 className="w-full h-full"
                 src="https://res.cloudinary.com/dd7fcqtnn/image/upload/v1739705997/img_oddaeq.png"
