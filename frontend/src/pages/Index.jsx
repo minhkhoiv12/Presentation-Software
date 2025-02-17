7b28b2c44cd354fd01aae678026f2fb9f16a16c7
websitecuakhoi <khoitk14033@gmail.com>
2025-02-17 11:16:12 +0700

    Setup Tailwindcss

---
+++ frontend/src/pages/Index.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+const Index = () => {
+  return (
+    <div className="bg-[#18191b] min-h-screen w-full ">
+      <div className="bg-[#212223] shadow-md">
+        <div className="w-[93%] m-auto py-3">
+          <div className="flex justify-between items-center">
+            <div className="w-[80px] h-[48px]">
+              <img
+                className="w-full h-full"
+                src="https://res.cloudinary.com/dd7fcqtnn/image/upload/v1739705997/img_oddaeq.png"
+                alt=""
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Index;
