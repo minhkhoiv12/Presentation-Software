db13015b3f2219ea634767c21b67782542a8572e
websitecuakhoi <khoitk14033@gmail.com>
2025-02-17 16:24:53 +0700

    Home Page Create

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,33 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Index from "./pages/Index";
+import Layout from "./pages/Layout";
+import Home from "./components/Home";
+import Templates from "./components/Templates";
+import Projects from "./components/Projects";
+import CreateDesign from "./components/CreateDesign";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <Index />,
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/templates",
+        element: <Templates />,
+      },
+      {
+        path: "/projects",
+        element: <Projects />,
+      },
+    ],
+  },
+  {
+    path: "/design/create",
+    element: <CreateDesign />,
   },
 ]);
 
---
+++ frontend/src/components/Templates.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+const Templates = () => {
+  return <div>tempaltes</div>;
+};
+export default Templates;
