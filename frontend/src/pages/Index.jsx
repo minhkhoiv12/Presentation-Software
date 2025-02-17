db78e8bf74d01be90d5260068a3ac94885ce1f83
websitecuakhoi <khoitk14033@gmail.com>
2025-02-17 11:21:08 +0700

    Deploy

--- frontend/src/pages/Index.jsx
+++ frontend/src/pages/Index.jsx
@@ -5,7 +5,7 @@ const Index = () => {
       <div className="bg-[#212223] shadow-md">
         <div className="w-[93%] m-auto py-3">
           <div className="flex justify-between items-center">
-            <div className="w-[80px] h-[48px]">
+            <div className="w-[100px] h-[100px]">
               <img
                 className="w-full h-full"
                 src="https://res.cloudinary.com/dd7fcqtnn/image/upload/v1739705997/img_oddaeq.png"
