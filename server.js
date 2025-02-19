be9e26d35b0bd01d03710f650cbb1eb55a981584
websitecuakhoi <khoitk14033@gmail.com>
2025-02-19 22:46:07 +0700

    change

--- server.js
+++ server.js
@@ -8,11 +8,13 @@ const path = require("path");
 dotenv.config();
 app.use(express.json());
 
-if (process.env.NODE_ENV === "local") {
+if (process.env.NODE_ENV === "production") {
   app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://presentation-pixora.vercel.app",
       credentials: true,
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"], // Cung cấp các phương thức HTTP
+      allowedHeaders: ["Content-Type", "Authorization"], // Cung cấp các header cần thiết cho request
     })
   );
 } else {
@@ -22,8 +24,7 @@ if (process.env.NODE_ENV === "local") {
     })
   );
 }
-
-// app.use('/api', require('./routes/designRoutes'))
+//app.use("/api", require("./routes/designRoutes"));
 app.use("/api", require("./routes/authRoutes"));
 
 if (process.env.NODE_ENV === "production") {
@@ -37,12 +38,12 @@ if (process.env.NODE_ENV === "production") {
 
 const dbConnect = async () => {
   try {
-    if (process.env.NODE_ENV === "production") {
-      await mongoose.connect(process.env.MONGODB_URI);
-      console.log("Production Database Is Connected..");
-    } else {
+    if (process.env.NODE_ENV === "local") {
       await mongoose.connect(process.env.LOCAL_DB_URI);
       console.log("Local Database Is Connected..");
+    } else {
+      await mongoose.connect(process.env.MONGODB_URI);
+      console.log("Production Database Is Connected..");
     }
   } catch (error) {
     console.log("Database connection Failed.");
